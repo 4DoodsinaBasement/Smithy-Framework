3784c5b8ba26940cb8c619d1a12c4f8537ddc825
nate.online <nate@travelwt.com>
2019-12-17 18:21:35 -0700

    Successfully removed the IGOAP interface

--- Assets/Baker.cs
+++ Assets/Baker.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Baker : MonoBehaviour, IGOAP
+public class Baker : GOAP_Agent
 {
 	public Backpack backpack;
 	public float moveSpeed = 1;
 
 	void Start()
 	{
+		Initialize();
 		backpack = GetComponent<Backpack>();
 	}
 
+	void Update()
+	{
+		UpdateStateMachine();
+	}
 
-	public HashSet<KeyValuePair<string, object>> CreateGoalState()
+
+	public override HashSet<KeyValuePair<string, object>> CreateGoalState()
 	{
 		HashSet<KeyValuePair<string, object>> goal = new HashSet<KeyValuePair<string, object>>();
 
@@ -21,7 +27,7 @@ public class Baker : MonoBehaviour, IGOAP
 		return goal;
 	}
 
-	public HashSet<KeyValuePair<string, object>> GetWorldState()
+	public override HashSet<KeyValuePair<string, object>> GetWorldState()
 	{
 		HashSet<KeyValuePair<string, object>> worldData = new HashSet<KeyValuePair<string, object>>();
 
@@ -31,32 +37,33 @@ public class Baker : MonoBehaviour, IGOAP
 		return worldData;
 	}
 
-	public void PlanFailed(HashSet<KeyValuePair<string, object>> failedGoal)
+	public override void PlanFailed(HashSet<KeyValuePair<string, object>> failedGoal)
 	{
 
 	}
 
-	public void PlanFound(HashSet<KeyValuePair<string, object>> goal, Queue<GOAP_Action> actions)
+	public override void PlanFound(HashSet<KeyValuePair<string, object>> goal, Queue<GOAP_Action> actions)
 	{
 
 	}
 
-	public void PlanFinished()
+	public override void PlanFinished()
 	{
 
 	}
 
-	public void PlanAborted(GOAP_Action aborter)
+	public override void PlanAborted(GOAP_Action aborter)
 	{
 
 	}
 
-	public bool MoveAgent(GOAP_Action nextAction)
+	public override bool MoveAgent(GOAP_Action nextAction)
 	{
+		// move towards the NextAction's target
 		float step = moveSpeed * Time.deltaTime;
-		transform.position = Vector3.MoveTowards(transform.position, nextAction.target.transform.position, step);
+		gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, nextAction.target.transform.position, step);
 
-		if (transform.position.Equals(nextAction.target.transform.position))
+		if (gameObject.transform.position.Equals(nextAction.target.transform.position))
 		{
 			// we are at the target location, we are done
 			nextAction.isInRange = true;
--- Assets/Smithy Framework/Generic/GOAP/GOAP_Agent.cs
+++ Assets/Smithy Framework/Generic/GOAP/GOAP_Agent.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GOAP_Agent : MonoBehaviour
+public abstract class GOAP_Agent : MonoBehaviour
 {
 	HashSet<GOAP_Action> availableActions;
 	Queue<GOAP_Action> currentActions;
 
-	IGOAP dataLink;
 	GOAP_Planner planner;
 
 	FSM stateMachine;
@@ -18,12 +17,22 @@ public class GOAP_Agent : MonoBehaviour
 
 	void Start()
 	{
+		Initialize();
+	}
+
+	void Update()
+	{
+		UpdateStateMachine();
+	}
+
+
+	public void Initialize()
+	{
 		availableActions = new HashSet<GOAP_Action>();
 		currentActions = new Queue<GOAP_Action>();
 		planner = new GOAP_Planner();
 		stateMachine = new FSM();
 
-		InitializeDataLink();
 		InitializeActions();
 
 		CreateIdleState();
@@ -33,25 +42,14 @@ public class GOAP_Agent : MonoBehaviour
 		stateMachine.pushState(idleState);
 	}
 
-	void Update()
+	public void UpdateStateMachine()
 	{
 		stateMachine.Update(gameObject);
 	}
 
+	#region Actions
 
-	void InitializeDataLink()
-	{
-		foreach (Component component in gameObject.GetComponents(typeof(Component)))
-		{
-			if (typeof(IGOAP).IsAssignableFrom(component.GetType()))
-			{
-				dataLink = (IGOAP)component;
-				return;
-			}
-		}
-	}
-
-	private void InitializeActions()
+	void InitializeActions()
 	{
 		GOAP_Action[] actions = gameObject.GetComponents<GOAP_Action>();
 		foreach (GOAP_Action action in actions)
@@ -60,17 +58,43 @@ public class GOAP_Agent : MonoBehaviour
 		}
 	}
 
+	public void AddAction(GOAP_Action action)
+	{
+		availableActions.Add(action);
+	}
+
+	public GOAP_Action GetAction()
+	{
+		foreach (GOAP_Action action in availableActions)
+		{
+			if (action.GetType().Equals(action)) { return action; }
+		}
+		return null;
+	}
+
+	public void RemoveAction(GOAP_Action action)
+	{
+		availableActions.Remove(action);
+	}
+
+	bool HasActionPlan
+	{
+		get { return currentActions.Count > 0; }
+	}
+
+	#endregion
+
 	#region FSM
 
-	private void CreateIdleState()
+	void CreateIdleState()
 	{
 		idleState = (fsm, gameObj) =>
 		{
 			// GOAP planning
 
 			// get the world state and the goal we want to plan for
-			HashSet<KeyValuePair<string, object>> worldState = dataLink.GetWorldState();
-			HashSet<KeyValuePair<string, object>> goal = dataLink.CreateGoalState();
+			HashSet<KeyValuePair<string, object>> worldState = GetWorldState();
+			HashSet<KeyValuePair<string, object>> goal = CreateGoalState();
 
 			// Plan
 			Queue<GOAP_Action> plan = planner.Plan(gameObject, availableActions, worldState, goal);
@@ -78,7 +102,7 @@ public class GOAP_Agent : MonoBehaviour
 			{
 				// we have a plan, hooray!
 				currentActions = plan;
-				dataLink.PlanFound(goal, plan);
+				PlanFound(goal, plan);
 
 				fsm.popState(); // move to PerformAction state
 				fsm.pushState(performState);
@@ -88,7 +112,7 @@ public class GOAP_Agent : MonoBehaviour
 			{
 				// ugh, we couldn't get a plan
 				Debug.Log("<color=orange>Failed Plan:</color>");
-				dataLink.PlanFailed(goal);
+				PlanFailed(goal);
 				fsm.popState(); // move back to IdleAction state
 				fsm.pushState(idleState);
 			}
@@ -104,7 +128,7 @@ public class GOAP_Agent : MonoBehaviour
 			GOAP_Action action = currentActions.Peek();
 			if (action.RequiresInRange() && action.target == null)
 			{
-				Debug.Log("<color=red>Fatal error:</color> Action requires a target but has none. Planning failed. You did not assign the target in your Action.CanBePerformed()");
+				Debug.Log("<color=red>Fatal error:</color> Action requires a target but has none. Planning failed. You did not assign the target in your Action.checkProceduralPrecondition()");
 				fsm.popState(); // move
 				fsm.popState(); // perform
 				fsm.pushState(idleState);
@@ -112,14 +136,32 @@ public class GOAP_Agent : MonoBehaviour
 			}
 
 			// get the agent to move itself
-			if (dataLink.MoveAgent(action))
+			if (MoveAgent(action))
 			{
 				fsm.popState();
 			}
+
+			/*MovableComponent movable = (MovableComponent) gameObj.GetComponent(typeof(MovableComponent));
+			if (movable == null) {
+				Debug.Log("<color=red>Fatal error:</color> Trying to move an Agent that doesn't have a MovableComponent. Please give it one.");
+				fsm.popState(); // move
+				fsm.popState(); // perform
+				fsm.pushState(idleState);
+				return;
+			}
+
+			float step = movable.moveSpeed * Time.deltaTime;
+			gameObj.transform.position = Vector3.MoveTowards(gameObj.transform.position, action.target.transform.position, step);
+
+			if (gameObj.transform.position.Equals(action.target.transform.position) ) {
+				// we are at the target location, we are done
+				action.setInRange(true);
+				fsm.popState();
+			}*/
 		};
 	}
 
-	private void CreatePerformState()
+	void CreatePerformState()
 	{
 		performState = (fsm, gameObj) =>
 		{
@@ -131,7 +173,7 @@ public class GOAP_Agent : MonoBehaviour
 				Debug.Log("<color=red>Done actions</color>");
 				fsm.popState();
 				fsm.pushState(idleState);
-				dataLink.PlanFinished();
+				PlanFinished();
 				return;
 			}
 
@@ -158,7 +200,7 @@ public class GOAP_Agent : MonoBehaviour
 						// action failed, we need to plan again
 						fsm.popState();
 						fsm.pushState(idleState);
-						dataLink.PlanAborted(action);
+						PlanAborted(action);
 					}
 				}
 				else
@@ -174,7 +216,7 @@ public class GOAP_Agent : MonoBehaviour
 				// no actions left, move to Plan state
 				fsm.popState();
 				fsm.pushState(idleState);
-				dataLink.PlanFinished();
+				PlanFinished();
 			}
 
 		};
@@ -182,29 +224,40 @@ public class GOAP_Agent : MonoBehaviour
 
 	#endregion
 
-	public void AddAction(GOAP_Action action)
-	{
-		availableActions.Add(action);
-	}
+	#region Interface
 
-	public GOAP_Action GetAction()
-	{
-		foreach (GOAP_Action action in availableActions)
-		{
-			if (action.GetType().Equals(action)) { return action; }
-		}
-		return null;
-	}
+	/// <summary>
+	/// The starting state of the Agent and the world. Supply what states are needed for actions to run.
+	/// </summary>
+	public abstract HashSet<KeyValuePair<string, object>> GetWorldState();
 
-	public void RemoveAction(GOAP_Action action)
-	{
-		availableActions.Remove(action);
-	}
+	/// <summary>
+	/// Give the planner a new goal so it can figure out the actions needed to fulfill it.
+	/// </summary>
+	public abstract HashSet<KeyValuePair<string, object>> CreateGoalState();
 
-	bool HasActionPlan
-	{
-		get { return currentActions.Count > 0; }
-	}
+	/// <summary>
+	/// No sequence of actions could be found for the supplied goal. You will need to try another goal.
+	/// </summary>
+	public abstract void PlanFailed(HashSet<KeyValuePair<string, object>> failedGoal);
+
+	/// <summary>
+	/// A plan was found for the supplied goal.  These are the actions the Agent will perform, in order.
+	/// </summary>
+	public abstract void PlanFound(HashSet<KeyValuePair<string, object>> goal, Queue<GOAP_Action> actions);
+
+	/// <summary>
+	/// All actions are complete and the goal was reached. Hooray!
+	/// </summary>
+	public abstract void PlanFinished();
+
+	/// <summary>
+	/// One of the actions caused the plan to abort. That action is returned.
+	/// </summary>
+	public abstract void PlanAborted(GOAP_Action aborter);
 
 
+	public abstract bool MoveAgent(GOAP_Action nextAction);
+
+	#endregion
 }
